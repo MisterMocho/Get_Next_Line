618365107510d98c41411f0368ab70765bb013cf
MisterMocho <lmcp95@gmail.com>
2025-12-03 21:41:42 +0000

    Fix buffer handling and line checking in get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -12,27 +12,39 @@
 
 #include "get_next_line.h"
 
+static char	*line_check(char *line)
+{
+	if (!line || line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE + 1];
 	char		*line;
 	ssize_t		bytes_read;
 
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE < 1)
+	if (fd < 0 || fd >= 1024 || BUFFER_SIZE < 1)
 		return (NULL);
 	line = NULL;
 	line = ft_strjoin_free(line, buffer);
 	while (!ft_strchr_nullc(buffer, '\n'))
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0 || buffer[0] == '\0')
-			return (free(line), new_buffer(buffer), NULL);
+		if (bytes_read < 0)
+		{
+			buffer[0] = '\0';
+			return (free(line), NULL);
+		}
 		if (!bytes_read)
 			break ;
-		if (bytes_read < BUFFER_SIZE)
-			buffer[bytes_read] = '\0';
+		buffer[bytes_read] = '\0';
 		line = ft_strjoin_free(line, buffer);
 	}
 	new_buffer(buffer);
-	return (line);
+	return (line_check(line));
 }
