8a76ae7c161e59052cd203a4bdd1673f1a529323
MisterMocho <lmcp95@gmail.com>
2025-12-03 21:42:54 +0000

    Fix boundary checks and line handling in get_next_line

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,27 +12,39 @@
 
 #include "get_next_line_bonus.h"
 
+static char	*line_check(char *line)
+{
+	if (!line || line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	buffer[1024][BUFFER_SIZE + 1];
 	char		*line;
 	ssize_t		bytes_read;
 
-	if (fd < 0 || fd > 1024 || BUFFER_SIZE < 1 || BUFFER_SIZE > 10000000)
+	if (fd < 0 || fd >= 1024 || BUFFER_SIZE < 1 || BUFFER_SIZE > 10000000)
 		return (NULL);
 	line = NULL;
 	line = ft_strjoin_free(line, buffer[fd]);
 	while (!ft_strchr_nullc(buffer[fd], '\n'))
 	{
 		bytes_read = read(fd, buffer[fd], BUFFER_SIZE);
-		if (bytes_read < 0 || buffer[fd][0] == '\0')
-			return (free(line), new_buffer(buffer[fd]), NULL);
+		if (bytes_read < 0)
+		{
+			buffer[fd][0] = '\0';
+			return (free(line), NULL);
+		}
 		if (!bytes_read)
 			break ;
-		if (bytes_read < BUFFER_SIZE)
-			buffer[fd][bytes_read] = '\0';
+		buffer[fd][bytes_read] = '\0';
 		line = ft_strjoin_free(line, buffer[fd]);
 	}
 	new_buffer(buffer[fd]);
-	return (line);
+	return (line_check(line));
 }
